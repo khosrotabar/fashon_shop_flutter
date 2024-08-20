94095da755a6c64f007b4ce55d7413b324c76cdc
khosro35 <mh.khosrotabar75@gmail.com>
2024-08-20 12:04:31 +0330

    store changes

---
+++ lib/src/cart/models/cart_count_model.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+CartCountModel cartCountModelFromJson(String str) => CartCountModel.fromJson(json.decode(str));
+
+String cartCountModelToJson(CartCountModel data) => json.encode(data.toJson());
+
+class CartCountModel {
+  final int cartCount;
+
+  CartCountModel({
+    required this.cartCount,
+  });
+
+  factory CartCountModel.fromJson(Map<String, dynamic> json) => CartCountModel(
+        cartCount: json["cart_count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "cart_count": cartCount,
+      };
+}
